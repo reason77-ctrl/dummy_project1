26f6a7fcf2c22ca4d4e762faef273a23105eba37
reason77-ctrl <reasonregmi27@gmail.com>
2024-05-07 12:38:57 +0545

    added gitignore

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    include_package_data= True,
+    name = "package_reason",
+    version= "2.1.0",
+    description= "Package_reason calculator module",
+    url = '',
+    author= 'Reason',
+    author_email= 'reasonregmi25@hotmail.com',
+    packages= ['package_reason'],
+    install_requires = [],
+    long_description= 'This is a dummy project for setup.py and modules of python',
+    long_description_content_type= 'text/markdown',
+)

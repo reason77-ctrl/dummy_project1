5a92613d4c312f139465e30a6a82b3161a5e509b
reason77-ctrl <reasonregmi27@gmail.com>
2024-05-24 17:36:35 +0545

    web_scrapping

--- package_web_scraping/laliga_table_scrap.py
+++ package_web_scraping/laliga_table_scrap.py
@@ -1,54 +1,24 @@
-from bs4 import BeautifulSoup
-import requests
+from requests_html import HTMLSession
 import json
-import csv
-import pandas as pd
 
-url = BeautifulSoup('https://www.laliga.com/en-GB/laliga-easports/standing', 'html.parser')
+s = HTMLSession()
 
-response = requests.get(url)
+url = 'https://www.laliga.com/en-GB/laliga-easports/standing'
 
-soup = response.text
-soup = BeautifulSoup(soup, "lxml")
+r = s.get(url)
 
-table_tag = soup.find("div", "show")
-# print(table_tag)
-
-tr_header = table_tag.find("div", "styled__StandingTableBody-sc-e89col-5")
-
-table_header = tr_header.find("div", class_="styled__Td-sc-e89col-10")
-
-all_tab_header = table_header.find_next_siblings("div")
-
-heading_data = []
-for i in all_tab_header:
-    heading_data.append(i.text)
-
-
-table_contents = table_tag.find_all("div", "styled__StandingTabBody-sc-e89col-0 isRHqh")
-
-final_output = []
-for tab_content in table_contents:
-    output = []
-    for child in tab_content.children:
-        output.append(child.text)
-    del output[0]
-    final_output.append(output)
-final_output.insert(0, heading_data)
+table = r.html.find('.show')[0]
 
 
 
-# with open('corona.csv','w') as f:
-#     x = csv.writer(f)
-#     for i in b:
-#         x.writerow(i)
 
+table_data = [[c.text for c in row.find('.styled__TextStyled-sc-1mby3k1-0')[2:]] for row in table.find('.styled__StandingTabBody-sc-e89col-0')]
 
+table_header = [[c.text for c in row.find('.styled__TextStyled-sc-1mby3k1-0')[1:]] for row in table.find('.styled__StandingTabHeader-sc-e89col-7')][0]
 
-# df = pd.read_csv('corona.csv', encoding = 'latin1',nrows = 50)
+result = [dict(zip(table_header, t)) for t in table_data]
 
-# print(df)
+with open('table.json', 'w') as data_file:
+    json.dump(result, data_file, indent=2)
 
-# with open("scrap_data.json", "w") as data_file:
-#     json.dump(b, data_file)
 
